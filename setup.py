77e58929fcf9a9f59617b8f0107a7773fe531143
topongo <lorenzo.bodini.private@gmail.com>
2020-09-06 15:52:57 +0200

    Version Update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
       name='telebot',
-      version='0.1',
+      version='0.2',
       description='',
       url='https://github.com/topongo/telebot',
       author='Lorenzo Bodini',
