2319ceb19b18764b51c1f53f3a5c9989be598172
topongo <lorenzo.bodini.private@gmail.com>
2020-09-06 22:58:49 +0200

    Publish package on PyPI Add distribution stuff to .gitignore Add license, description, platform and long_description to package-info
    Version jump due to issues with PyPI

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
 import setuptools
 
+with open("README.md") as f:
+      LONGDESCRIPTION = f.read()
+
 setuptools.setup(
       name='telebotapi',
-      version='0.2',
-      description='',
+      version='0.4',
+      description="A telegram bot api for telegram, with asynchronous polling",
       url='https://github.com/topongo/telebotapi',
       author='Lorenzo Bodini',
       author_email='lorenzo.bodini.private@gmail.com',
       packages=['telebotapi'],
       python_requires='>=3.6',
+      license="GPL3",
+      platform="All",
+      long_description=LONGDESCRIPTION,
+      long_description_content_type="text/markdown",
 )
